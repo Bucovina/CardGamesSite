a919e3ac2023b7d8c1a11921282829f6f35282fd
Bucovina <103101131+Bucovina@users.noreply.github.com>
2023-03-15 22:28:57 +0200

    Etapa 3

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+app = express();
+console.log("Folder",__dirname);
+
+app.get("/ceva", function (req, res) {
+    res.send("altceva")
+})
+
+app.get("/index.html", function (req, res) {
+    res.sendFile(__dirname+"/index.html")
+})
+
+
+app.listen(8080);
+console.log("Server started!");
