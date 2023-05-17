21c7eae491f7a1b2b801d9f384e2088ae2ed2186
Bucovina <103101131+Bucovina@users.noreply.github.com>
2023-05-17 19:16:07 +0300

    Etapa 6

--- index.js
+++ index.js
@@ -1,16 +1,247 @@
 const express = require("express");
+const fs = require('fs');
+const path = require('path');
+const sharp = require('sharp');
+const sass = require('sass');
+
+const {Client} = require('pg');
+
+const AccesBD = require('./module_proprii/accesbd.js');
+
+AccesBD.getInstanta().select({
+        tabel: "produse",
+        campuri: ["nume", "pret", "componente"],
+        conditiiAnd: ["pret>20"]
+    }, function (err, rez) {
+        console.log(err);
+        console.log(rez);
+    }
+)
+
+var client = new Client({
+    database: "CardGamesSite", user: "postgres", password: "spatula", host: "localhost", port: 5432
+});
+client.connect();
+client.query("select * from produse", function (err, rez) {
+    /// console.log("Eroare BD", err);
+
+    ///console.log("Rezultat BD", rez.rows);
+});
+
+obiectGlobal = {
+    obErori: null,
+    obImagini: null,
+    folderScss: path.join(__dirname, "resurse/scss"),
+    folderCss: path.join(__dirname, "resurse/css"),
+    folderBackup: path.join(__dirname, "backup"),
+    optiuniMeniu: [],
+}
+
+client.query("select * from unnest(enum_range(null::categorie_produse))", function (err, rezCategorie) {
+    if (err) {
+        console.log(err);
+    } else {
+        obiectGlobal.optiuniMeniu = rezCategorie.rows;
+    }
+});
 
 app = express();
-console.log("Folder",__dirname);
+//console.log("Folder", __dirname);
+//console.log("Cale fisier", __filename);
+console.log("Director de lucru", process.cwd());
+
+vectorFoldere = ["temp", "temp1", "backup"]
+for (let folder of vectorFoldere) {
+    // let caleFolder=__dirname+"/"+folder;
+    let caleFolder = path.join(__dirname, folder);
+    if (!fs.existsSync(caleFolder)) {
+        fs.mkdirSync(caleFolder);
+    }
+}
+
+function compileazaScss(caleScss, caleCss) {
+    if (!caleCss) {
+        // let vectorCale = caleScss.split("\\");
+        // let numeFisExt = vectorCale[vectorCale.length - 1];
+        let numeFisExt = path.basename(caleScss);
+        let numeFis = numeFisExt.split(".")[0];
+        caleCss = numeFis + ".css";
+    }
+    if (!path.isAbsolute(caleScss)) caleScss = path.join(obiectGlobal.folderScss, caleScss)
+    if (!path.isAbsolute(caleCss)) caleCss = path.join(obiectGlobal.folderCss, caleCss)
+    // let vectorCale = caleCss.split("\\");
+    // let numeFisCss = vectorCale[vectorCale.length - 1];
+    let numeFisCss = path.basename(caleCss);
+    let caleBackup = path.join(obiectGlobal.folderBackup, "resurse/css")
+    if (!fs.existsSync(caleBackup)) {
+        fs.mkdirSync(caleBackup, {recursive: true});
+    }
+    if (fs.existsSync(caleCss)) {
+        fs.copyFileSync(caleCss, path.join(obiectGlobal.folderBackup, "resurse/css", numeFisCss))
+    }
+    rez = sass.compile(caleScss, {"sourceMap": true});
+    fs.writeFileSync(caleCss, rez.css);
+    /// console.log("Compilare SCSS", rez);
+}
+
+vFisiere = fs.readdirSync(obiectGlobal.folderScss);
+for (let numeFis of vFisiere) {
+    if (path.extname(numeFis) === '.scss') {
+        compileazaScss(numeFis);
+    }
+}
+
+fs.watch(obiectGlobal.folderScss, function (eveniment, numeFis) {
+    /// console.log(eveniment, numeFis);
+    if (eveniment === "change" || eveniment === "rename") {
+        let caleCompleta = path.join(obiectGlobal.folderScss, numeFis);
+        if (fs.existsSync(caleCompleta)) {
+            compileazaScss(caleCompleta);
+        }
+    }
+});
+
+app.set("view engine", "ejs");
+
+app.use("/resurse", express.static(__dirname + "/resurse"));
+
+app.use("/node_modules", express.static(__dirname + "/node_modules"));
+
+app.use("/*", function (req, res, next) {
+    res.locals.optiuniMeniu = obiectGlobal.optiuniMeniu;
+    next();
+})
+app.use(/^\/resurse(\/[a-zA-Z0-9]*)*$/, function (req, res) {
+    afisareEroare(res, 403);
+});
+
+app.get("/favicon.ico", function (req, res) {
+    res.sendFile(__dirname + "/resurse/ico/favicon.ico");
+});
 
 app.get("/ceva", function (req, res) {
-    res.send("altceva")
+    console.log("cale:", req.url);
+    res.send("altceva ip:" + req.ip);
 })
 
-app.get("/index.html", function (req, res) {
-    res.sendFile(__dirname+"/index.html")
+app.get(["/index", "/", "/home"], function (req, res) {
+    res.render("pagini/index", {ip: req.ip, a: 15, b: 12, imagini: obiectGlobal.obImagini.imagini});
 })
 
+app.get("/produse", function (req, res) {
+    //TO DO query pentru a selecta toate produsele
+    //TO DO se adauaga filtrarea dupa tipul produsului
+    //TO DO se selecteaza si toate valorile din enum-ul categ_prajitura
+
+
+    client.query("select * from unnest(enum_range(null::categorie_produse))", function (err, rezCategorie) {
+        if (err) {
+            console.log(err);
+        } else {
+            client.query("select * from unnest(enum_range(null::producatori))", function (err, rezProducatori) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    let conditieWhere = "";
+                    if (req.query.tip) conditieWhere = `where categorie_produs='${req.query.tip}'`;
+                    client.query("select * from produse "+ conditieWhere, function (err, rez) {
+                        console.log(300)
+                        if (err) {
+                            console.log(err);
+                            afisareEroare(res, 2);
+                        } else res.render("pagini/produse", {
+                            produse: rez.rows, optiuni: rezCategorie.rows, produc: rezProducatori.rows,
+                        });
+                    });
+                }
+            })
+        }
+    })
+});
+
+app.get("/produs/:id", function (req, res) {
+    client.query(`select * from produse where id = ${req.params.id}`, function (err, rezultat) {
+        if (err) {
+            console.log(err);
+            afisareEroare(res, 2);
+        } else res.render("pagini/produs", {prod: rezultat.rows[0]});
+    });
+});
+
+//^\w+\.ejs$
+app.get("/*.ejs", function (req, res) {
+    afisareEroare(res, 400);
+})
+
+app.get("/*", function (req, res) {
+    try {
+        res.render("pagini" + req.url, function (err, rezRandare) {
+            if (err) {
+                console.log(err);
+                if (err.message.startsWith("Failed to lookup view")) afisareEroare(res, 404); else afisareEroare(res);
+            } else {
+                res.send(rezRandare);
+            }
+        });
+    } catch (err) {
+        if (err.message.startsWith("Cannot find module")) afisareEroare(res, 404); else afisareEroare(res);
+    }
+})
+
+function initErori() {
+    var continut = fs.readFileSync(__dirname + "/resurse/json/erori.json").toString("utf-8");
+    obiectGlobal.obErori = JSON.parse(continut);
+    let vErori = obiectGlobal.obErori.info_erori;
+    ///for (let i = 0; i < vErori.length; i++)
+    for (let eroare of vErori) {
+        eroare.imagine = "/" + obiectGlobal.obErori.cale_baza + "/" + eroare.imagine;
+    }
+}
+
+initErori();
+
+function initImagini() {
+    var continut = fs.readFileSync(__dirname + "/resurse/json/galerie.json").toString("utf-8");
+    obiectGlobal.obImagini = JSON.parse(continut);
+    let vImagini = obiectGlobal.obImagini.imagini;
+    let caleAbs = path.join(__dirname, obiectGlobal.obImagini.cale_galerie);
+    let caleAbsMediu = path.join(caleAbs, "mediu");
+    if (!fs.existsSync(caleAbsMediu)) {
+        fs.mkdirSync(caleAbsMediu);
+    }
+    ///for (let i = 0; i < vErori.length; i++)
+    for (let imag of vImagini) {
+        [numeFis, ext] = imag.cale_fisier.split(".");
+        let caleFisAbs = path.join(caleAbs, imag.cale_fisier);
+        let caleFisMediuAbs = path.join(caleAbsMediu, numeFis + ".webp");
+        sharp(caleFisAbs).resize(400).toFile(caleFisMediuAbs);
+        imag.fisier_mediu = "/" + path.join(obiectGlobal.obImagini.cale_galerie, "mediu", numeFis + ".webp")
+        imag.cale_fisier = "/" + path.join(obiectGlobal.obImagini.cale_galerie, imag.cale_fisier);
+        ///imag.imagine = "/" + obiectGlobal.obErori.cale_baza + "/" + eroare.imagine;
+    }
+}
+
+initImagini();
+
+function afisareEroare(res, _identificator, _titlu = "titlu default", _text, _imagine) {
+    let vErori = obiectGlobal.obErori.info_erori;
+    let eroare = vErori.find(function (elem) {
+        return elem.identificator === _identificator;
+    })
+    if (eroare) {
+        let titlu1 = _titlu === "titlu default" ? (eroare.titlu || _titlu) : _titlu;
+        let text1 = _text || eroare.text
+        let imagine1 = _imagine || eroare.imagine
+        if (eroare.status) res.status(eroare.identificator).render("pagini/eroare", {
+            titlu: titlu1, text: text1, imagine: imagine1
+        }); else res.render("pagini/eroare", {titlu: titlu1, text: text1, imagine: imagine1});
+    } else {
+        let errDef = obiectGlobal.obErori.eroare_default;
+        res.render("pagini/eroare", {
+            titlu: errDef.titlu, text: errDef.text, imagine: obiectGlobal.obErori.cale_baza + "/" + errDef.imagine
+        })
+    }
+}
 
 app.listen(8080);
 console.log("Server started!");
---
+++ resurse/js/produse.js
@@ -0,0 +1,214 @@
+window.addEventListener("load", function () {
+    document.getElementById("inp-puncte").onchange = function () {
+        document.getElementById("infoRange").innerHTML = `(${this.value})`;
+    }
+
+    document.getElementById("filtrare").onclick = function () {
+        let val_nume = document.getElementById("inp-nume").value.toLowerCase();
+        let radiobuttons = document.getElementsByName("gr_rad");
+        let producator;
+        for (let r of radiobuttons) {
+            if (r.checked) {
+                producator = r.value;
+                break;
+            }
+        }
+
+        let var_puncte = document.getElementById("inp-puncte").value;
+
+        let val_categ = document.getElementById("inp-categorie").value;
+
+        let val_material = document.getElementById("inp-material").value;
+
+        let val_colectie = document.getElementById("inp-colectie").checked.toString();
+
+        let val_descriere = document.getElementById("inp-descriere").value.toLowerCase();
+
+        let vector_desc = val_descriere.split(" ");
+
+        let plus_desc = [];
+        let minus_desc = [];
+
+        for (let vec of vector_desc) {
+            if (vec.trim().startsWith("+"))
+                plus_desc.push(vec.split("+")[1]);
+            else if (vec.trim().startsWith("-"))
+                minus_desc.push(vec.split("-")[1]);
+        }
+
+        let val_pret=document.getElementById("inp-pret");
+
+        let pret_select=[];
+
+        for (let pret of val_pret){
+            if(pret.selected){
+                pret_select.push(pret.value);
+            }
+        }
+
+        let pret_min=[];
+        let pret_max=[];
+
+        for (let pret of pret_select){
+            pret_min.push(parseInt(pret.split('-')[0]));
+            pret_max.push(parseInt(pret.split('-')[1]));
+        }
+
+        var produse = document.getElementsByClassName("produs");
+
+        for (let prod of produse) {
+            prod.style.display = "none";
+            let nume = prod.getElementsByClassName("val-nume")[0].innerHTML.toLowerCase();
+
+            let cond1 = (nume.includes(val_nume));
+
+            let producatori = prod.getElementsByClassName("val-producator")[0].innerHTML;
+
+            let cond2 = (producator === "toate" || producator === producatori);
+
+            let puncte = parseFloat(prod.getElementsByClassName("val-puncte")[0].innerHTML);
+
+            let cond3 = (puncte > var_puncte)
+
+            let categorie = prod.getElementsByClassName("val-categorie")[0].innerHTML;
+
+            let cond4 = (val_categ === "toate" || val_categ === categorie)
+
+            let material = prod.getElementsByClassName("val-material")[0].innerHTML;
+
+            let cond5 = (val_material === "" || val_material === material)
+
+            let colectie = prod.getElementsByClassName("val-colectie")[0].innerHTML;
+
+            let cond6 = (val_colectie === colectie)
+
+            let descriere = prod.getElementsByClassName("val-descriere")[0].innerHTML.toLowerCase();
+
+            let cond7=true;
+
+            for (let plus of plus_desc) {
+                cond7=false;
+                if (descriere.includes(' '+plus) || descriere.includes(plus+' ')) {
+                    cond7 = true;
+                    break;
+                }
+            }
+
+            for (let minus of minus_desc) {
+                if (descriere.includes(' '+minus) || descriere.includes(minus+' ')) {
+                    cond7 = false;
+                    break;
+                }
+            }
+
+            if(val_descriere==='')
+                cond7=true;
+
+            let pret=parseFloat(prod.getElementsByClassName("val-pret")[0].innerHTML);
+
+            let cond8=false;
+
+            for (let i=0; i<pret_min.length; i++)
+            {
+                if(pret_min[i]<=pret && pret<=pret_max[i]) {
+                    cond8 = true;
+                    break;
+                }
+            }
+
+            if (pret_select.length===0)
+                cond8=true;
+
+            if (cond1 && cond2 && cond3 && cond4 && cond5 && cond6 && cond7 && cond8) {
+                prod.style.display = "block";
+            }
+        }
+    }
+
+    document.getElementById("resetare").onclick = function () {
+
+        if (confirm("Sigur vrei sa resetezi?")) {
+            document.getElementById("inp-nume").value = "";
+            document.getElementById("r_default").checked = true;
+            document.getElementById("inp-pret").selectedIndex = -1;
+            document.getElementById("inp-pret").options.selected = false;
+            document.getElementById("inp-puncte").value = document.getElementById("inp-puncte").min;
+            document.getElementById("infoRange").innerHTML = `(0)`;
+            document.getElementById("inp-descriere").value = "";
+            document.getElementById("inp-categorie").value = "toate";
+            document.getElementById("inp-colectie").checked = false;
+            document.getElementById("inp-material").value = "";
+
+            var produse = document.getElementsByClassName("produs");
+
+            for (let prod of produse) {
+                prod.style.display = "block";
+            }
+
+            location.reload();
+        }
+    }
+
+    function sortare(semn) {
+        var produse = document.getElementsByClassName("produs");
+        var v_produse = Array.from(produse);
+        v_produse.sort(function (a, b) {
+            let pret_a = parseFloat(a.getElementsByClassName("val-pret")[0].innerHTML);
+            let pret_b = parseFloat(b.getElementsByClassName("val-pret")[0].innerHTML);
+            if (pret_a === pret_b) {
+                let nume_a = a.getElementsByClassName("val-categorie")[0].innerHTML;
+                let nume_b = b.getElementsByClassName("val-categorie")[0].innerHTML;
+                return semn * nume_a.localeCompare(nume_b);
+            }
+            return semn * (pret_a - pret_b);
+        });
+        for (let prod of v_produse) {
+            prod.parentElement.appendChild(prod);
+        }
+    }
+
+    document.getElementById("sortCrescNume").onclick = function () {
+        sortare(1);
+    }
+    document.getElementById("sortDescrescNume").onclick = function () {
+        sortare(-1);
+    }
+
+    window.onkeydown = function (e) {
+        if (e.key === "c" && e.altKey) {
+            if (document.getElementById("info-suma"))
+                return;
+            var produse = document.getElementsByClassName("produs");
+            let suma = 0;
+            for (let prod of produse) {
+                if (prod.style.display !== "none") {
+                    let pret = parseFloat(prod.getElementsByClassName("val-pret")[0].innerHTML)
+                    suma += pret;
+                }
+            }
+            let p = document.createElement("p");
+            p.innerHTML = suma;
+            p.id = 'info-suma';
+            ps = document.getElementById("p-suma");
+            container = ps.parentNode;
+            frate = ps.nextElementSibling;
+            container.insertBefore(p, frate);
+            setTimeout(function () {
+                let info = document.getElementById("info-suma");
+                if (info)
+                    info.remove()
+            }, 1000)
+        }
+    }
+
+   document.getElementById("inp-colectie").onchange=function toggleButtonClass() {
+       val_colectie = document.getElementById("inp-colectie");
+        if (val_colectie.checked) {
+            document.getElementById("label-colectie").classList.remove("btn-outline-secondary");
+            document.getElementById("label-colectie").classList.add("btn-secondary");
+        } else {
+            document.getElementById("label-colectie").classList.remove("btn-secondary");
+            document.getElementById("label-colectie").classList.add("btn-outline-secondary");
+        }
+    }
+})
